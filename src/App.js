e9580a175e4ddfc912e278ad0ae858f6f2e725cf
Dubeyratneshar <Dubeyratneshvar12@gmail.com>
2023-09-10 15:53:05 +0530

    my first commmit reactwebsite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 import './App.css';
-
 function App() {
+  const [isHiddenPassword, setIsHiddenPassword] = useState(true);
+  const [password, setPassword] = useState("");
+  const renderEyesPassword = () => {
+    if (!password) {
+      return;
+    }
+    if (isHiddenPassword) {
+      return (
+        <AiFillEye
+          onClick={() => setIsHiddenPassword((prev) => !prev)}
+          className="iconEyes"
+        />
+      );
+    }
+    if (!isHiddenPassword) {
+      return (
+        <AiFillEyeInvisible
+          onClick={() => setIsHiddenPassword((prev) => !prev)}
+          className="iconEyes"
+        />
+      );
+    }
+  };
+
+  const handleOnchange = (e) => {
+    setPassword(e.target.value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="login">
+      <h1 className="loginTitle">🔏Signup🔏</h1>
+      <form className="loginForm">
+        <div className="loginFormInputGroup">
+        <label htmlFor="email">Email</label>
+          <input placeholder="Enter email...." id="email" name="email" />
+        </div>
+        <div className="loginFormInputGroup">
+          <label htmlFor="password">Password</label>
+          <div className="relative">
+            <input
+            onChange={handleOnchange}
+            type={isHiddenPassword ? "password" : "text"}
+              placeholder="Enter password..."
+              id="password"
+              name="password"
+            />
+            {renderEyesPassword()}
+          </div>
+        </div>
+        <button className="loginFormBtnLogin">Login</button>
+      </form>
+    </div>);
 }
 
 export default App;
